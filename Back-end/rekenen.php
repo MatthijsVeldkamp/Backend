80f97f539cad6869a26d2dfb5f850454239e38ac
Matthijs Veldkamp <99071402@mydavinci.nl>
2024-03-27 11:18:00 +0100

    Rekenen optimized en inplaatsvan 72 staat er nu 8 x 9 = 72

--- Back-end/rekenen.php
+++ Back-end/rekenen.php
@@ -1,15 +1,12 @@
 <?php
-function multiply($n1, $n2) {
-    return ($n1 * $n2);
-}
 function generateMultiplicationTable($numbers) {
     foreach ($numbers as $number) {
         for ($j = 1; $j <= 10; $j++) {
-            echo multiply($number, $j) . "<br>";
+            echo "$number x $j = " . ($number * $j) . "<br>";
         }
         echo "<br>";
     }
 }
-$numbers = [3,5,8,12];
+$numbers = [3, 5, 8, 12];
 generateMultiplicationTable($numbers);
 ?>
